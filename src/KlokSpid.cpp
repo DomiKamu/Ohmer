3d30ebf68e54834f997ee3c4f7d78fc9e69496b2
DomiKamu <docam@free.fr>
2023-01-24 00:58:48 +0100

    v2.2.3 - Dark room compliant

--- src/KlokSpid.cpp
+++ src/KlokSpid.cpp
@@ -1609,8 +1609,24 @@ struct KlokSpidDMD : TransparentWidget {
 
 	void drawLayer(const DrawArgs &args, int layer) override {
 		if (layer == 1) {
-			if (!(font = APP->window->loadFont(fontPath))) {
-					return;
+			if (!(font = APP->window->loadFont(fontPath)))
+				return;
+			// Yellow rounded rectangle to simulate yellow backlit of (LCD) dot-matrix display ("Absolute Night" model only).
+			if (module) {
+				if (module->Theme == 2) {
+					// Main DMD.
+					nvgBeginPath(args.vg);
+			    nvgRoundedRect(args.vg, 7.16f, 43.6f, 105.7f, 45.48f, 6.5f);
+			    nvgFillColor(args.vg, nvgRGBA(0xC0, 0xE9, 0x10, 0xff));
+			    nvgFill(args.vg);
+					nvgClosePath(args.vg);
+					// Lower DMD.
+					nvgBeginPath(args.vg);
+			    nvgRoundedRect(args.vg, 33.14f, 282.95f, 53.71f, 29.35f, 3.3f);
+			    nvgFillColor(args.vg, nvgRGBA(0xC0, 0xE9, 0x10, 0xff));
+			    nvgFill(args.vg);
+					nvgClosePath(args.vg);
+				}
 			}
 			// Main DMD, upper line.
 			nvgFontSize(args.vg, 16);
